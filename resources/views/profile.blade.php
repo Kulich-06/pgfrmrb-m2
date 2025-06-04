2b41b99ffd5114ae3e73da30cc154d8a2697175a
Kulich <nasti_2005@list.ru>
2025-06-04 21:04:39 +0500

    reg2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,7 +13,7 @@
                 Профиль пользователя
             </h4>
 
-            @if(Auth::check())
+            @if(Auth::check() && Auth::user())
                 <p class="mb-3 text-center fs-5">
                     <strong>Имя:</strong> <span class="text-light">{{ Auth::user()->name }}</span>
                 </p>
@@ -31,7 +31,9 @@
                     {{ Auth::user()->created_at->format('d.m.Y H:i') }}
                 </p>
             @else
-                <p class="text-center text-warning fs-5">Вы не авторизованы. <a href="{{ route('login') }}" class="text-decoration-underline text-info">Войдите</a>, чтобы просмотреть профиль.</p>
+                <p class="text-center text-warning fs-5">
+                    Вы не авторизованы. <a href="{{ route('login') }}" class="text-info text-decoration-underline">Войти</a>
+                </p>
             @endif
 
         </div>
@@ -39,4 +41,3 @@
 </div>
 
 @endsection
-
